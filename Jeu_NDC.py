a238139617bf261f8fede5e2875b55761d6c3fa1
MK <pminhkhue1308@gmail.com>
2023-05-24 15:28:55 +0200

    ouverture d'une fenetre

--- Jeu_NDC.py
+++ Jeu_NDC.py
@@ -4,16 +4,28 @@ import pyxel, random
 
 class Jeu:
     def __init__(self):
-        pyxel.init(128, 128, title="Nuit du Code")
+        pyxel.init(128, 128, title="Nuit du Code", fps = 60)
+        pyxel.run(self.update, self.draw)
+        pyxel.load(filename = "images.pyxres")
+        self.x = 0
+
+    def update(self) :
+        if pyxel.btnp(pyxel.KEY_Q) :
+            pyxel.quit()
+        
+
+    def draw(self) :
+        pyxel.cls(0)
+        pyxel.rect(60, 0, 8, 8, 9)
         
  
 
 
 class Perso :
-    def __init__(x, y) :
-        player_img = pyxel.blt(121, 11, 0, 6, 8)
-        player_x = x
-        player_y = y
+    def __init__(self,x, y) :
+        self.player_img = pyxel.blt(121, 11, 0, 6, 8)
+        self.player_x = x
+        self.player_y = y
 
     def vaisseau_deplacement(x, y):
 
