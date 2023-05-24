021eca6e4e07007901f35df79beeb83d70765d75
TBLR <133204678+TBLR1@users.noreply.github.com>
2023-05-24 15:27:56 +0200

    Update Jeu_NDC.py

--- Jeu_NDC.py
+++ Jeu_NDC.py
@@ -1,14 +1,16 @@
 import pyxel, random
 
-#test
-
 class Jeu:
     def __init__(self):
         pyxel.init(128, 128, title="Nuit du Code")
+        pyxel.load("images.pyxres")
         pyxel.run(self.update, self.draw)
-        
- 
 
+    def draw():
+        pyxel.cls
+
+    def update(self):
+        pass
 
 class Perso :
     def __init__(x, y) :
