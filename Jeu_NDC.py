3116d7ffc1a43b2db57016df75b01089dedb3b35
TBLR1 <133204678+TBLR1@users.noreply.github.com>
2023-05-24 14:47:46 +0200

    Update Jeu_NDC.py

--- Jeu_NDC.py
+++ Jeu_NDC.py
@@ -3,3 +3,16 @@ import pyxel, random
 class Jeu:
     def __init__(self):
         pyxel.init(128, 128, title="Nuit du Code")
+        self.score = 100
+        self.vies = 3
+        
+
+
+initialisation du Jeu
+mouvement perso
+environnement
+obstacles / bâteaux
+compteur de pas
+collision
+ecran défaite/victoire
+
