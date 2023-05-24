ccb7e490f0af2926ff306e15a93062718ee4d581
TBLR1 <133204678+TBLR1@users.noreply.github.com>
2023-05-24 14:32:34 +0200

    Update Jeu_NDC.py

--- Jeu_NDC.py
+++ Jeu_NDC.py
@@ -1,7 +1,13 @@
-import pyxel, random
+import pyxel
 
-#test
+pyxel.init(160, 120)
 
-class Jeu:
-    def __init__(self):
-        pyxel.init(128, 128, title="Nuit du Code")
+def update():
+    if pyxel.btnp(pyxel.KEY_Q):
+        pyxel.quit()
+
+def draw():
+    pyxel.cls(0)
+    pyxel.rect(10, 10, 20, 20, 11)
+
+pyxel.run(update, draw)
