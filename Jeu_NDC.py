83ee950c9b0d3a3b43e454d647f4f50a2d8d7d5f
MK <pminhkhue1308@gmail.com>
2023-05-24 16:12:36 +0200

    classe perso

--- Jeu_NDC.py
+++ Jeu_NDC.py
@@ -1,37 +1,16 @@
 import pyxel, random
 
-class Jeu:
-    def __init__(self):
-
-        pyxel.init(128, 128, title="Nuit du Code", fps = 60)
-        pyxel.run(self.update, self.draw)
-        pyxel.load(filename = "images.pyxres")
-        self.x = 0
-
-    def update(self) :
-        if pyxel.btnp(pyxel.KEY_Q) :
-            pyxel.quit()
-        
+class Perso :
+    def __init__(self, x, y) :
+        self.player_x = x
+        self.player_y = y
 
     def draw(self) :
         pyxel.cls(0)
         pyxel.rect(60, 0, 8, 8, 9)
-        
- 
 
-    def draw():
-        pyxel.cls
 
-    def update(self):
-        pass
-
-class Perso :
-    def __init__(self,x, y) :
-        self.player_img = pyxel.blt(121, 11, 0, 6, 8)
-        self.player_x = x
-        self.player_y = y
-
-    def vaisseau_deplacement(x, y):
+    def perso_deplacement(x, y):
 
         if pyxel.btn(pyxel.KEY_RIGHT):
             if (x < 120) :
@@ -47,6 +26,24 @@ class Perso :
                 y = y - 1
         return x, y
 
+
+class Jeu:
+    def __init__(self):
+        pyxel.init(128, 128, title="Nuit du Code", fps = 60)
+        pyxel.load(filename = "images.pyxres")
+        self.perso = Perso(60, 60)
+        pyxel.run(self.update(), self.draw())
+
+
+    def update(self) :
+        if pyxel.btnp(pyxel.KEY_Q) :
+            pyxel.quit()
+
+    def draw(self) :
+        pyxel.cls(0)
+        pyxel.rect(60, 0, 8, 8, 9)
+        self.perso.draw()
+        
 #pyxel.load(filename="images.pyxres")
 
 Jeu()
